2fafd6f98a09bee4f0c3a84b25f37a817166fcd2
RASQUIN <yannymk@hotmail.com>
2021-02-16 10:10:13 +0100

    initial commit

---
+++ bin/main.dart
@@ -0,0 +1,28 @@
+// SIngle comment
+/* Multiline comment*/
+
+void main() {
+  print("Hello World");
+
+  // define a number variable INT
+  int score = 23;
+  var score1 = 24;
+  int hexValue = 0xEADEBAEE;
+  int score2; //value of object by default is NULL
+
+  // define a number variable DOUBLE
+  double percentage = 93.4;
+  var percent = 94;
+  double exponents = 1.42e5;
+
+  // define a String
+  String name = "Lizzy";
+  var surname = "Princesa";
+
+  // define a boolean
+  bool isValid = true;
+  var isaCat = true;
+
+  print(score2);
+  print(exponents);
+}
